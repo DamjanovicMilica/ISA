f89a4e4f500830a7e3582d6a795396034e99e0aa
Milica <Milica@DESKTOP-BMHOU8M>
2018-09-16 18:57:51 +0200

    dodan repozitorijum za gosta

---
+++ BioskopPoyyy/src/main/java/com/BioskopPoyyy/repository/guest/GuestRepository.java
@@ -0,0 +1,28 @@
+package com.BioskopPoyyy.repository.guest;
+
+
+import java.util.List;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.BioskopPoyyy.model.Guest;
+import com.BioskopPoyyy.model.UserRoles;
+
+
+
+@Repository
+public interface GuestRepository extends JpaRepository<Guest, Long> {
+
+	
+	List<Guest> findByAddress(String address);
+	Guest findByEmail(String email);
+	List<Guest> findByName(String name);
+	Guest findByEmailAndPassword(String username, String password);
+	List<Guest> findByPassword(String password);
+
+	List<Guest> findByRole(UserRoles role);
+	List<Guest> findBySurname(String surname);
+	
+	
+}
